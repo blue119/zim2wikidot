8cfb8768e2f80e1d75e75a8d4bcd23b864a65db0
Chun-Yu Lee (Mat) matlinuxer2@gmail.com <matlinuxer2@gmail.com>
2010-10-21 21:08:56 +0800

    The first prototype for zim custom tools.

---
+++ zim_tools.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+f = open( sys.argv[1], 'a+')
+
+f.write( "%f: " + sys.argv[1] + "\n" )
+f.write( "%d: " + sys.argv[2] + "\n" )
+f.write( "%s: " + sys.argv[3] + "\n" )
+f.write( "%n: " + sys.argv[4] + "\n" )
+f.write( "%D: " + sys.argv[5] + "\n" )
+f.write( "%t: " + sys.argv[6] + "\n" )
+
+f.close()
